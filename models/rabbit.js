be0bbc64ed659505c54f872711cf1ff603eb3bb7
harshitha gongidi <g9harshitha@gmail.com>
2022-04-13 13:37:59 -0500

    changed

--- models/rabbit.js
+++ models/rabbit.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const rabbitSchema = mongoose.Schema({
-    age: String,
-    color: String,
+    age:{
+        type: String,
+    },
+    color:{
+        type: String,
+    },
     weight: String
 })
 module.exports = mongoose.model("Rabbit", rabbitSchema)
