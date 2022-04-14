f6060e6732b9e550b631a45c38bf5697b14b771e
harshitha gongidi <g9harshitha@gmail.com>
2022-04-13 21:11:33 -0500

    mongo

--- app.js
+++ app.js
@@ -81,4 +81,4 @@ var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function(){
-console.log("Connection to DB succeeded")});
+  console.log("Connection to DB succeeded")});
