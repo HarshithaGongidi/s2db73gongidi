f18303f5678f0fba4450e7a15293800f56a5aa0b
harshitha gongidi <g9harshitha@gmail.com>
2022-04-15 07:19:38 -0500

    controller

--- controllers/rabbit.js
+++ controllers/rabbit.js
@@ -26,8 +26,15 @@ exports.rabbit_view_all_Page = async function(req, res) {
 };
 
 // for a specific Rabbit.
-exports.rabbit_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: rabbit detail: ' + req.params.id);
+exports.rabbit_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await rabbit.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 
 // Handle rabbit create on POST.
